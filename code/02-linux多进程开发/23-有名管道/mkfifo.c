37f31f490f6e5c1f32ef4059c414adea179d7bbf
kazemu <487253379@qq.com>
2023-06-05 22:32:45 +0800

    mkfifo rdwr

---
+++ code/02-linux多进程开发/23-有名管道/mkfifo.c
@@ -0,0 +1,61 @@
+/*
+    创建fifo文件
+    1.通过命令： mkfifo 名字
+    2.通过函数：int mkfifo(const char *pathname, mode_t mode);
+
+    #include <sys/types.h>
+    #include <sys/stat.h>
+    int mkfifo(const char *pathname, mode_t mode);
+        参数：
+            - pathname: 管道名称的路径
+            - mode: 文件的权限 和 open 的 mode 是一样的
+                    是一个八进制的数
+        返回值：成功返回0，失败返回-1，并设置错误号
+
+*/
+
+/*
+    管道的注意事项：
+        1.一个为只读而打开一个管道的进程会阻塞，直到另外一个进程为只写打开管道
+        2.一个为只写而打开一个管道的进程会阻塞，直到另外一个进程为只读打开管道
+
+    读管道：
+        管道中有数据，read返回实际读到的字节数
+        管道中无数据：
+            管道写端被全部关闭，read返回0，（相当于读到文件末尾）
+            写端没有全部被关闭，read阻塞等待
+    
+    写管道：
+        管道读端被全部关闭，进行异常终止（收到一个SIGPIPE信号）
+        管道读端没有全部关闭：
+            管道已经满了，write会阻塞
+            管道没有满，write将数据写入，并返回实际写入的字节数。
+*/
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+
+
+    // 判断文件是否存在
+    int ret = access("fifo1", F_OK);
+    if(ret == -1) {
+        printf("管道不存在，创建管道\n");
+        
+        ret = mkfifo("fifo1", 0664);
+
+        if(ret == -1) {
+            perror("mkfifo");
+            exit(0);
+        }       
+
+    }
+
+    
+
+    return 0;
+}
