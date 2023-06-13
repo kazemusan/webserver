d1857102c872feeb414af51f1882d980bf2b4909
kazemu <487253379@qq.com>
2023-06-13 14:49:09 +0800

    fcntl

--- code/01-linux系统编程入门/16-文件描述符操作/fcntl.c
+++ code/01-linux系统编程入门/16-文件描述符操作/fcntl.c
@@ -15,11 +15,32 @@
 
             - F_SETFL : 设置文件描述符文件状态flag
               必选项：O_RDONLY, O_WRONLY, O_RDWR 不可以被修改
-              可选性：O_APPEND, O)NONBLOCK
+              可选性：O_APPEND, O_NONBLOCK
                 O_APPEND 表示追加数据
                 NONBLOK 设置成非阻塞
         
         阻塞和非阻塞：描述的是函数调用的行为。
+
+        返回值：
+            - 成功：返回对应的操作结果，如F_DUPFD返回的是新的文件描述符
+            - 失败：-1
+
+    文件状态：
+        文件状态是由文件描述符的文件状态flag决定的。
+        文件状态flag是通过open函数的第二个参数来设置的。
+        文件状态flag是一个int类型的值，这个值是由多个选项通过或运算得到的。
+        文件状态flag的选项：
+            - 必选项：O_RDONLY, O_WRONLY, O_RDWR
+            - 可选项：O_APPEND, O_NONBLOCK
+        文件状态flag的获取和设置：
+            - 获取：fcntl(fd, F_GETFL);
+            - 设置：fcntl(fd, F_SETFL, flag);
+        文件状态flag的修改：
+            - 必选项：不可以被修改
+            - 可选项：可以被修改
+        文件状态flag的作用：
+            - 必选项：必须要指定，否则open函数会报错
+            - 可选项：可以不指定，如果不指定，那么就使用默认值
 */
 
 #include <unistd.h>
